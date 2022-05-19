0f490413c7a46c431c455ab9aa3745a748691e3c
blangka <chaehong73@gmai.com>
2022-05-19 13:24:47 +0900

    git ignore추가, 챕터3 테스트 추가

---
+++ test/ct3/go.mod
@@ -0,0 +1,3 @@
+module github.com/blangka/Go-Programming-Cookbook-Second-Edition/test/ct3
+
+go 1.18
---
+++ test/ct3/main.go
@@ -0,0 +1,7 @@
+package ct3
+
+import "github.com/blangka/Go-Programming-Cookbook-Second-Edition/test/ct3"
+
+func main() {
+	ct3.ShowConv()
+}
